2b9fdfdb485ab8c7f45a398f20298f4232269497
emrullahd69 <69113305+emrullahd69@users.noreply.github.com>
2020-12-05 22:50:10 +0200

    Update GithubStudy.java
    remote to local Emrullah

--- src/gitHub/GithubStudy.java
+++ src/gitHub/GithubStudy.java
@@ -3,5 +3,6 @@ package gitHub;
 public class GithubStudy {
     public static void main(String[] args) {
         System.out.println("Start Github Study");
+        System.out.println("Remote to Local Emrullah");
     }
 }
